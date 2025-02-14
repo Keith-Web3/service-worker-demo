16c4eba126775f2901ed33db98dd53ef9fbdaf3c
olamilekan@root <olorunnisholaolamilekan@gmail.com>
2025-02-14 12:14:26 +0100

    feat: notifications working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Handler } from 'express'
 import webPush from 'web-push'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { Pool } from 'pg'
 
 dotenv.config()
 const app = express()
@@ -12,6 +13,22 @@ app.use(
     origin: '*', // Allow all origins
   })
 )
+
+const pool = new Pool({
+  connectionString: process.env['POSTGRES_URI'],
+})
+
+const createSubscriptionTable = async () => {
+  const data = await pool.query(`
+    CREATE TABLE IF NOT EXISTS subscriptions (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL UNIQUE,
+    expirationTime TIMESTAMP DEFAULT NOW() + INTERVAL '1 hour',
+    subscription JSONB NOT NULL
+  );`)
+  console.log({ data })
+}
+createSubscriptionTable()
 
 webPush.setVapidDetails(
   'mailto:olorunnisholaolamilekan@gmail.com',
@@ -27,21 +44,56 @@ const subscribeHandler: Handler = async function (req, res) {
     return
   }
 
-  // Payload for the notification
-  const payload = JSON.stringify({
-    title: 'Test Notification',
-    message: 'This is a test push notification.',
-  })
+  // Save the subscription to the database
+  const data = await pool.query(
+    `
+    INSERT INTO subscriptions (user_id, subscription)
+    VALUES ($1, $2)
+    ON CONFLICT (user_id)
+    DO UPDATE SET 
+      subscription = $2,
+      expirationTime = NOW() + INTERVAL '1 hour';
+    `,
+    [1, subscription]
+  )
+  console.log('saved subscription!', { data })
+  res.status(201).json({ message: 'Subscription saved successfully!' })
+}
+
+const notifyHandler: Handler = async function (req, res) {
+  const { message, title } = req.body
+  const userId = req.params['id']
+  if (!message || !title) {
+    res.status(400).json({ error: 'Message and title are required' })
+    return
+  }
+
+  // Get the user's subscription from the database
+  const data = await pool.query(
+    `SELECT * FROM subscriptions WHERE user_id = $1`,
+    [userId]
+  )
+  console.log({ data })
+  const subscription = data.rows[0].subscription
+
+  if (!subscription) {
+    res.status(400).json({ error: 'Subscription not found' })
+    return
+  }
 
   // Send the notification to the subscribed user
   try {
-    await webPush.sendNotification(subscription, payload)
+    await webPush.sendNotification(
+      subscription,
+      JSON.stringify({ title, message })
+    )
     res.status(200).json({ message: 'Notification sent successfully!' })
   } catch (err) {
     console.error('Error sending notification', err)
   }
 }
 app.post('/api/subscribe', subscribeHandler)
+app.post('/api/notify/:id', notifyHandler)
 
 // Start the server
 app.listen(3000, () => {
--- frontend/sw.js
+++ frontend/sw.js
@@ -1,32 +1,39 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installing...");
+self.addEventListener('install', event => {
+  console.log('Service Worker installing...')
   event.waitUntil(
-    caches.open("static-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/index.js", "./icon.png"]);
+    caches.open('static-cache').then(cache => {
+      return cache.addAll(['/', '/index.html', '/index.js', './icon.png'])
     })
-  );
-});
+  )
+})
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated.");
-});
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated.')
+})
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
     })
-  );
-});
+  )
+})
 
 // Handle push notifications (server-triggered)
-self.addEventListener("push", (event) => {
-  console.log("Push event received:", event);
+self.addEventListener('push', event => {
+  console.log('Push event received:', event)
+
+  let notificationData = {
+    title: 'New Notification',
+    message: 'Placeholder notification.',
+  }
+
+  if (event.data) notificationData = event.data.json() // Parse the received data
   const options = {
-    body: "This is a push notification!",
-    icon: "/icon.png",
-  };
+    body: notificationData.message,
+    icon: '/icon.png',
+  }
   event.waitUntil(
-    self.registration.showNotification("Push Notification", options)
-  );
-});
+    self.registration.showNotification(notificationData.title, options)
+  )
+})
